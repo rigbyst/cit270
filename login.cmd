a5d481dba60324ce3621c3ab6e691b7b08ad8e8b
rigbyst <rigbys@byui.edu>
2023-06-14 13:50:56 -0600

    update

--- login.cmd
+++ login.cmd
@@ -1,6 +1,6 @@
 echo "logging in"
 
-curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://localhost:3000/login
+curl --insecure -v -d "@login.json" POST -H "Content-Type:application/json" http://brorigby.cit270.com/login
 
-rem curl -v https://dev.stedi.me/validate/f803f12f-bc5f-4896-913d-1edd16d9e102
+
 
